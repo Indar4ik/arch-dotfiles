5e2208d961f5199ef20c2835dadb76c466dfcc20
Indar4ik <pozdnovcube@gmail.com>
2026-06-16 22:35:39 +0300

    modified:   .config/hypr/appearance.lua

--- .config/hypr/appearance.lua
+++ .config/hypr/appearance.lua
@@ -8,22 +8,25 @@ hl.config({
     decoration = {
         rounding = 14,
         rounding_power = 2,
+
         active_opacity = 1.0,
         inactive_opacity = 0.9,
+
         shadow = {
-        enabled = false,
+            enabled = false,
         },
+
         blur = {
-        enabled = true,
-        size = 3,
-        passes = 4,
-        vibrancy = 0.15,
-        vibrancy_darkness = 2.5,
-        -- noise = 0.008,
-        -- contrast = 0.9,
-        brightness = 0.9,
-        popups = true,
-        popups_ignorealpha = 0.3,
+            enabled = true,
+            size = 3,
+            passes = 4,
+            vibrancy = 0.15,
+            vibrancy_darkness = 2.5,
+            -- noise = 0.008,
+            -- contrast = 0.9,
+            brightness = 0.9,
+            popups = true,
+            popups_ignorealpha = 0.3,
         },
     },
 
