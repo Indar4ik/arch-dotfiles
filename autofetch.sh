3ac3e290971a27aef8c584ed507f611ab094c2c0
Indar4ik <pozdnovcube@gmail.com>
2026-06-26 12:37:06 +0300

    autofetch.sh

---
+++ autofetch.sh
@@ -0,0 +1,12 @@
+cp -rf $XDG_CONFIG_HOME/hypr .config/
+cp -rf $XDG_CONFIG_HOME/niri .config/
+cp -rf $XDG_CONFIG_HOME/kitty .config/
+cp -rf $XDG_CONFIG_HOME/alacritty .config/
+cp -rf $XDG_CONFIG_HOME/fish .config/
+cp -rf $XDG_CONFIG_HOME/fastfetch .config/
+cp -rf $XDG_CONFIG_HOME/btop .config/
+cp -rf $XDG_CONFIG_HOME/tmux .config/
+cp -rf $XDG_CONFIG_HOME/rofi .config/
+cp $HOME/.bashrc ./
+cp $HOME/.zshrc ./
+cp $HOME/.vimrc ./
