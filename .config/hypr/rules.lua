261a65f26024e82d03185e2ffd3ee79061351622
Indar4ik <pozdnovcube@gmail.com>
2026-07-04 20:24:23 +0300

    изменено:      .config/hypr/rules.lua

--- .config/hypr/rules.lua
+++ .config/hypr/rules.lua
@@ -12,3 +12,4 @@ hl.workspace_rule({ workspace = "special:coding", no_rounding = true, decorate =
 
 hl.window_rule({ match = { class = "Alacritty" }, rounding = 0, no_blur = true, opacity = "1.0 1.0 override"})
 hl.window_rule({ match = { class = "mpv" }, fullscreen = true, rounding = 0, no_blur = true, opacity = "1.0 1.0 override"})
+hl.window_rule({ match = { class = "balls-in-circle" }, fullscreen = true, rounding = 0, no_blur = true, opacity = "1.0 1.0 override"})
