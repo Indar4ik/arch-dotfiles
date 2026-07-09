bf6a35c10fad75a0cf335711400eae8bb54cf928
Indar4ik <pozdnovcube@gmail.com>
2026-07-09 15:07:49 +0300

    modified:   autofetch.sh

--- autofetch.sh
+++ autofetch.sh
@@ -1,12 +1,12 @@
-cp -rf $XDG_CONFIG_HOME/hypr .config/
-cp -rf $XDG_CONFIG_HOME/niri .config/
-cp -rf $XDG_CONFIG_HOME/kitty .config/
-cp -rf $XDG_CONFIG_HOME/alacritty .config/
-cp -rf $XDG_CONFIG_HOME/fish .config/
-cp -rf $XDG_CONFIG_HOME/fastfetch .config/
-cp -rf $XDG_CONFIG_HOME/btop .config/
-cp -rf $XDG_CONFIG_HOME/tmux .config/
-cp -rf $XDG_CONFIG_HOME/rofi .config/
+cp -r $XDG_CONFIG_HOME/hypr .config/
+cp -r $XDG_CONFIG_HOME/niri .config/
+cp -r $XDG_CONFIG_HOME/kitty .config/
+cp -r $XDG_CONFIG_HOME/alacritty .config/
+cp -r $XDG_CONFIG_HOME/fish .config/
+cp -r $XDG_CONFIG_HOME/fastfetch .config/
+cp -r $XDG_CONFIG_HOME/btop .config/
+cp -r $XDG_CONFIG_HOME/tmux .config/
+cp -r $XDG_CONFIG_HOME/rofi .config/
 cp $HOME/.bashrc ./
 cp $HOME/.zshrc ./
 cp $HOME/.vimrc ./
