342b4a0ca9dad1ba432b38d78cab9383bf2d8b47
Indar4ik <pozdnovcube@gmail.com>
2026-07-25 22:24:18 +0300

    lite appearance && rename alt anim

---
+++ .config/hypr/appearance_lite.lua
@@ -0,0 +1,24 @@
+hl.config({
+    general = {
+        gaps_in = 2,
+        gaps_out = 3,
+        border_size = 1,
+    },
+  
+    decoration = {
+        rounding = 14,
+        rounding_power = 2,
+
+        shadow = {
+            enabled = false,
+        },
+
+        blur = {
+            enabled = false,
+        },
+    },
+
+    animations = {
+        enabled = false,
+    }
+})
