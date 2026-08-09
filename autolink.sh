2cd5443c2f553d59ca498bcb4a06e72d380c33b7
Indar4ik <pozdnovcube@gmail.com>
2026-08-09 10:25:30 +0300

    added autolink script

---
+++ autolink.sh
@@ -0,0 +1,17 @@
+ln -sf $(pwd)/.bashrc $HOME/
+ln -sf $(pwd)/.zshrc $HOME/
+ln -sf $(pwd)/.vimrc $HOME/
+ln -sf $(pwd)/.p10k.zsh $HOME/
+ln -sf $(pwd)/.config/kitty $HOME/.config/
+ln -sf $(pwd)/.config/alacrity $HOME/.config/
+ln -sf $(pwd)/.config/fish $HOME/.config/
+ln -sf $(pwd)/.config/fastfetch $HOME/.config/
+ln -sf $(pwd)/.config/btop $HOME/.config/
+ln -sf $(pwd)/.config/tmux $HOME/.config/
+ln -sf $(pwd)/.config/rofi $HOME/.config/
+ln -sf $(pwd)/.config/cava $HOME/.config/
+ln -sf $(pwd)/.config/yazi $HOME/.config/
+ln -sf $(pwd)/.config/elio $HOME/.config/
+
+ln -sf $(pwd)/.config/hypr $HOME/.config/
+ln -sf $(pwd)/.config/niri $HOME/.config/
